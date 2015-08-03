bea86e79322c7aa17d8301040c400b604f88e3d8
LucoLe <lyudmilles@gmail.com>
2015-08-03 11:09:12 +0200

    implement winner_combination and check_winner methods
    winner_combination method checks the array for winning positions and
    the check_winner method gives the state of the game - whether the game is
    still in progress, if there is a winner or the game is draw.
    RSpec spec are updated to check the functionality.

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -27,4 +27,41 @@ class Game
   def place_marker(cell_nr, marker)
     @board[cell_nr - 1] = marker
   end
+
+  def winner_combination
+    combination = nil
+    # Check rows
+    (0..8).step(3) do |i|
+      if @board[i] != "-" && @board[i] == @board[i+1] && @board[i] == @board[i+2]
+        return combination = [i+1, i+2, i+3]
+      end
+    end
+
+    # Check columns
+    (0..2).each do |i|
+      if @board[i] != "-" && @board[i] == @board[i+3] && @board[i] == @board[i+6]
+        return combination = [i+1, i+4, i+7]
+      end
+    end
+
+    # Check top left - bottom right diag
+    if @board[0] != "-" && @board[0] == @board[4] && @board[0] == @board[8]
+      return combination = [1, 5, 9]
+    end
+
+    # Check top right - bottom left diag
+    if @board[2] != "-" && @board[2] == @board[4] && @board[2] == @board[6]
+      return combination = [3, 5, 7]
+    end
+  end
+
+  def check_winner
+    winner = nil
+    if !winner_combination && !@board.include?("-")
+      winner = "draw"
+    elsif winner_combination
+      winner = winner_combination
+    end
+    winner
+  end
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,10 +1,17 @@
 require 'tictactoe'
 describe Game do
-  let(:testBoard) { ["X", "X", "O", "-", "O", "-", "O", "-", "X"] }
+  let(:emptyBoard) { ["-", "-", "-", "-", "-", "-", "-", "-", "-"] }
+  let(:testBoard) { ["X", "X", "O", "-", "O", "-", "X", "-", "O"] }
+  let(:draw) { ["X", "O", "X", "X", "O", "O", "O", "X", "X"] }
+  let(:middle_row) { ["O", "-", "O", "X", "X", "X", "-", "-", "-"] }
+  let(:last_col) { ["O", "-", "X", "-", "-", "X", "O", "-", "X"] }
+  let(:top_left_bottom_right) { ["O", "-", "X", "O", "X", "-", "X", "-", "-"] }
+  let(:top_right_bottom_left) { ["X", "-", "O", "O", "X", "-", "-", "-", "X"] }
+
   describe "initialize" do
     it "without arguments" do
       game = Game.new()
-      expect(game.board).to eq(["-", "-", "-", "-", "-", "-", "-", "-", "-"])
+      expect(game.board).to eq(emptyBoard)
     end
 
     it "with state array" do
@@ -24,6 +31,44 @@ describe Game do
       expect(game.valid_move?(1)).to eq(false)
       expect(game.valid_move?(5)).to eq(false)
       expect(game.valid_move?("a5asdf")).to eq(false)
+    end
+  end
+
+  describe "win position" do
+    it "should be nil and game still in progress" do
+      game = Game.new(testBoard)
+      expect(game.winner_combination).to eq(nil)
+      expect(game.check_winner).to eq(nil)
+    end
+
+    it "should be draw" do
+      game = Game.new(draw)
+      expect(game.winner_combination).to eq(nil)
+      expect(game.check_winner).to eq("draw")
+    end
+
+    it "should be middle row" do
+      game = Game.new(middle_row)
+      expect(game.winner_combination).to eq([4, 5, 6])
+      expect(game.check_winner).to eq([4, 5, 6])
+    end
+
+    it "should be last column" do
+      game = Game.new(last_col)
+      expect(game.winner_combination).to eq([3, 6, 9])
+      expect(game.check_winner).to eq([3, 6, 9])
+    end
+
+    it "should be top left bottom right diagonal" do
+      game = Game.new(top_left_bottom_right)
+      expect(game.winner_combination).to eq([3, 5, 7])
+      expect(game.check_winner).to eq([3, 5, 7])
+    end
+
+    it "should be top right bottom left diagonal" do
+      game = Game.new(top_right_bottom_left)
+      expect(game.winner_combination).to eq([1, 5, 9])
+      expect(game.check_winner).to eq([1, 5, 9])
     end
   end
 end
